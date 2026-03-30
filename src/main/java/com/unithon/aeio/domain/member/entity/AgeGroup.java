272d597e7ed1309d2efd580a638a2707748ffae7
김혜령 <hyeryoung27@soongsil.ac.kr>
2026-03-30 22:24:50 +0900

    fix: 연령대 변경

--- src/main/java/com/unithon/aeio/domain/member/entity/AgeGroup.java
+++ src/main/java/com/unithon/aeio/domain/member/entity/AgeGroup.java
@@ -4,5 +4,7 @@ public enum AgeGroup {
     TEENS,       // 10대
     TWENTIES,    // 20대
     THIRTIES,    // 30대
-    FORTIES_PLUS // 40대 이상
+    FORTIES,    // 40대
+    FIFTIES,    // 50대
+    SIXTIES_PLUS    // 60대 이상
 }
